7e921e66bc42e6c5e0df2502b82e9033f20dfc79
Nate Banas <nate.banas@gmail.com>
2023-01-31 23:16:43 +0100

    created simple Task List app

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -12,9 +13,21 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
     @task.save
     redirect_to task_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
   end
 
   private
@@ -22,4 +35,8 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:title, :details)
   end
+end
+
+def set_task
+  @task = Task.find(params[:id])
 end
